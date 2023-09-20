5913df1fe147a20a863b902402f77c6a4f29264a
carlo tamayo <cejota708@gmail.com>
2023-09-20 09:57:07 -0600

    02 final

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -1,34 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package practica1;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Practica1 {
-    
-        private static int factorial(int num){
-        System.out.println("Estoy en factorial, num vale: "+num);
-        if (num==0){
-            return 1;
-        }
-        else {
-            return num* factorial(num-1);
-}
-        }
-    
-    
-    
-    public static void main(String[] args) {
+    public static BufferedReader bufer=new BufferedReader(new InputStreamReader(System.in));
+    public static String entrada;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Ingrese el valor de x: ");
+        int x;
+        entrada = bufer.readLine();
+        x = Integer.parseInt(entrada);
         
-    int numero=5;
-    int resultado;
-    
-    System.out.println("");
-    resultado = factorial(numero);
-    System.out.println("El factorial de "+numero+" es: "+resultado);
-    
+        System.out.println("Ingrese el valor de y: ");
+        int y;
+        entrada = bufer.readLine();
+        y = Integer.parseInt(entrada);
+        
+        System.out.println("Ingrese el valor de z: ");
+        int z;
+        entrada = bufer.readLine();
+        z = Integer.parseInt(entrada);
+
+        int resultado = sumaCuadrados(x, y, z);
+        System.out.println("La suma de los números elevados al cuadrado es: " + resultado);
     }
+ // public static int sumaCuadrados(int y, int z. Es la función recursiva que calcula la suma de los números elevados al cuadrado
+    public static int sumaCuadrados(int x, int y, int z) {
+        if (x > z) {
+            return 0;
+        } else {
+            int cuadradoActual= y * y;
+            return cuadradoActual + sumaCuadrados(x + 1, y, z);
+ }
 }
-        
-    
+}
